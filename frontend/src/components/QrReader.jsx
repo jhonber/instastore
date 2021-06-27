e07dbc4c1a93f2cebc2e1fff843be49dced6e59e
Jhon Jimenez <jhonb3r@gmail.com>
2021-06-27 18:33:23 -0500

    Improve styles (4)

--- frontend/src/components/QrReader.jsx
+++ frontend/src/components/QrReader.jsx
@@ -47,6 +47,8 @@ const QrReader = ({ handleQr, handleQrError, style }) => {
         width={1280}
         height={720}
         videoConstraints={{
+          width: 1280,
+          height: 720,
           facingMode: 'environment',
         }}
         style={style}
